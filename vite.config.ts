383b8bd62f07b1d2c03f9e0dfa5edf824645cfe6
norbiros <norbiros@protonmail.com>
2026-08-14 20:11:35 +0200

    feat: initialize the repository

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  pack: {
+    dts: {
+      tsgo: true,
+    },
+    exports: true,
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+      denyWarnings: true,
+    },
+  },
+  fmt: {},
+});
